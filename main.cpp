d5c334c748ad588939ea3097333a27d72d5221a6
chaoskoosa <chaoskoosa@gmail.com>
2016-09-21 09:48:45 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: saman
+ *
+ * Created on September 20, 2016, 12:20 AM
+ */
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/* Filling a 3D 30 col array
+ * 
+ */
+int main(int argc, char** argv) {
+    int SIZE = 30;
+    char wthr[SIZE][SIZE][SIZE];
+    
+    
+    return 0;
+}
+
